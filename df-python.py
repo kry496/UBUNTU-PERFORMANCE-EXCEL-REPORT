32d57e9305c6fff08532c53cf69f88f6beea06a9
kry496 <kry496@my.utsa.edu>
2017-04-02 04:18:28 -0500

    sample csv outputs

---
+++ df-python.py
@@ -0,0 +1,13 @@
+# python file for VMSTAT 
+import csv
+from subprocess import check_output
+
+def read_vmstat_output():
+    with open('test.csv', 'wb') as file_excel:
+        for x in check_output(['df','-h']).strip().splitlines():
+            wr = csv.writer(file_excel, delimiter=' ')
+            wr.writerow(x.split())
+
+
+read_vmstat_output()
+
